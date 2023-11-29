d75c3ee073d9fdd8f095fb48c9782be216f224ba
gmskdbcj <gmskdbcj@gmail.com>
2023-11-29 16:16:33 +0700

    update

--- handlers/basic_router.py
+++ handlers/basic_router.py
@@ -2,9 +2,24 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
+from scripts.rooms import Room
+
+import asyncio
+
 router = Router()
 
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer("Тест")
+    await message.answer("Поиск соперника...")
+    user_id = message.from_user.id
+    async with Room("data_bases/bot_db.db") as room:
+        opponent = await room.find()
+    if opponent == None:
+        async with Room("data_bases/bot_db.db") as room:
+            await room.create(user_id)
+    else:
+        async with Room("data_bases/bot_db.db") as room:
+            await room.join(user_id, opponent)
+        await message.answer("Cоперник найден.")
+        await message.bot.send_message(opponent, "Cоперник найден.")
---
+++ scripts/rooms.py
@@ -0,0 +1,30 @@
+import aiosqlite
+import asyncio
+
+class Room:
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.lock = asyncio.Lock()
+
+    async def __aenter__(self):
+        self.db = await aiosqlite.connect(self.db_file)
+        await self.db.execute("CREATE TABLE IF NOT EXISTS Rooms (player_1 INT PRIMARY KEY , player_2 INT)")
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.db.close()
+
+    async def find(self):
+        async with self.lock:
+            async with self.db.execute("SELECT player_1, player_2 FROM Rooms WHERE player_2 = ?", (0,)) as cursor:
+                return await cursor.fetchone()
+
+    async def create(self, player_id):
+        async with self.lock:
+            await self.db.execute("INSERT INTO Rooms (player_1, player_2) VALUES (?, ?)", (player_id, 0))
+            await self.db.commit()
+
+    async def join(self, my_id, id):
+        async with self.lock:
+            await self.db.execute("UPDATE Rooms SET player_2 = ? WHERE player_1 = ?", (my_id, id))
+            await self.db.commit()
